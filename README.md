8419974b36d1e43efd63a57a4f93e7cb09169d6d
Bierchermüesli <sgr@nts.ch>
2024-01-15 10:41:52 +0100

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dns-ping
 Ping like tool for DNS
+
+
+```
+./dns-bench.py asdf.com -s 1.1.1.1 -n 0.5 -t MX 
+asdf.com. 292 IN MX 10 mx10.asdf.com.    dns_seq=35889 time=0.0426s
+asdf.com. 292 IN MX 10 mx10.asdf.com.    dns_seq=64551 time=0.03688s
+asdf.com. 291 IN MX 10 mx10.asdf.com.    dns_seq=30070 time=0.05855s
+asdf.com. 291 IN MX 10 mx10.asdf.com.    dns_seq=57240 time=0.03662s
+^C
+--- asdf.com DNS statistics ---
+4 queries performed for asdf.com
+rtt min/avg/max/mdev 0.0366/0.0437/0.0586s
+```
---
+++ dns-bench.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# Stefan Grosser 2024
+
+import argparse
+from dns.resolver import Resolver
+import time
+
+
+def perform_dns_query(resolver, domain, type="A"):
+    try:
+        start_time = time.time()
+        r = resolver.resolve(domain, type)
+        end_time = time.time()
+        return end_time - start_time, r
+    except Exception as e:
+        return None, e
+
+
+def main():
+    parser = argparse.ArgumentParser(description="DNS Query Ping Like Tool")
+    parser.add_argument("-n", "--interval", type=float, default=1.0, help="Interval between queries in seconds")
+    parser.add_argument("-t", "--type", type=str, default="A", help="Type of Query A, MX, AAAA... ", choices=["A", "AAAA", "MX", "TXT", "SOA"])
+    parser.add_argument("-s", "--server", type=str, default=None, help="Server to use, default System DNS")
+    parser.add_argument("-v", "--verbose", type=int, default=0, help="make it verbose")
+    parser.add_argument("-l", "--lifetime", type=int, default=5, help="DNS LifeTime(out)")
+    parser.add_argument("domain", type=str, help="The domain to query", default="example.com")
+    args = parser.parse_args()
+
+    total_time = 0
+    query_count = 0
+    min_time = float("inf")
+    max_time = float("-inf")
+    resolver = Resolver()
+    resolver.timeout = args.lifetime
+    resolver.lifetime = args.lifetime
+    if args.server:
+        resolver.nameservers = [args.server]
+
+    try:
+        while True:
+            query_time, r = perform_dns_query(resolver, args.domain, args.type)
+
+            if query_time is not None:
+                query_count += 1
+                total_time += query_time
+                min_time = min(min_time, query_time)
+                max_time = max(max_time, query_time)
+                average_time = total_time / query_count
+                responses = len(r.response.answer[0].items)
+
+                if responses == 1:
+                    print(f"{str(r.response.answer[0]):<40} dns_seq={r.response.id:<5} time={query_time:.4}s")
+                elif (responses) > 1:
+                    print(f"{responses} Records dns_seq={r.response.id:<5} time={query_time:.4}s")
+                    if args.verbose:
+                        print(f"{r.response.answer[0]}")
+            else:
+                print(r)
+
+            time.sleep(args.interval)
+
+    except KeyboardInterrupt:
+
+        print(f"\n--- {args.domain} DNS statistics ---")
+        if query_count > 0:
+            print("{} queries performed for {}".format(query_count, args.domain))
+            print(f"rtt min/avg/max/mdev {min_time:.4f}/{total_time / query_count:.4f}/{max_time:.4f}s")
+        else:
+            print("No successful queries were performed.")
+
+
+if __name__ == "__main__":
+    main()
